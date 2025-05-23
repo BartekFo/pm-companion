f31792264b23b9fe309f18969a0c32d97818ff20
Bartosz Fórmanowski <bartosz.f@masterborn.com>
2025-05-23 23:18:58 +0200

    fix: update project page to include authentication and create project button

--- app/(project)/page.tsx
+++ app/(project)/page.tsx
@@ -1,12 +1,23 @@
+import { redirect } from 'next/navigation';
+import { CreateProjectButton } from './components/create-project-button';
 import { ProjectHeading } from './components/project-heading';
+import { auth } from '../(auth)/auth';
 
-export default function ProjectPage() {
+export default async function ProjectPage() {
+  const session = await auth();
+
+  if (!session) {
+    redirect('/login');
+  }
   return (
-    <div className="main-gradient h-dvh flex items-center justify-center">
+    <>
       <ProjectHeading
         title="Welcome to PM Companion!"
         description="Choose an existing project or create a new one to get started"
       />
-    </div>
+      <div className="flex flex-wrap rounded-sm gap-2.5 p-6 border border-primary-200 max-w-5xl w-full min-h-104 bg-[#FFFFFF66]">
+        <CreateProjectButton />
+      </div>
+    </>
   );
 }
