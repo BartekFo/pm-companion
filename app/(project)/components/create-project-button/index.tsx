01cad9c02ebd9a5e7d5614b8c7e3562ead250cea
Bartosz Fórmanowski <bartosz.f@masterborn.com>
2025-05-23 23:26:00 +0200

    fix: change border style from dotted to dashed in CreateProjectButton

--- app/(project)/components/create-project-button/index.tsx
+++ app/(project)/components/create-project-button/index.tsx
@@ -6,7 +6,7 @@ import { ROUTES } from '@/lib/constants/routes';
 export function CreateProjectButton() {
   return (
     <Link href={ROUTES.PROJECT.CREATE_NEW_PROJECT}>
-      <div className="flex flex-col items-center justify-center gap-1 w-55 h-45 rounded-sm border border-dotted border-primary-300 bg-neutral-0 hover:border-primary-900 group cursor-pointer active:bg-primary-50">
+      <div className="flex flex-col items-center justify-center gap-1 w-55 h-45 rounded-sm border border-dashed border-primary-300 bg-neutral-0 hover:border-primary-900 group cursor-pointer active:bg-primary-50">
         <SquarePlus className="text-primary-900 group-hover:fill-primary-900 group-hover:[&>path]:text-neutral-0" />
         <SmallText>Create new project</SmallText>
       </div>
