ea5d25230a9fcae3f4fe0f1fa4ac8336219188bc
Bartosz Fórmanowski <bartosz.f@masterborn.com>
2025-05-29 23:24:48 +0200

    feat: add migration for goofy spectrum and snapshot updates - Created SQL migration to drop tables: Document, Message, Suggestion, and Vote. - Added metadata for migration version 0016_goofy_spectrum in _journal.json. - Introduced 0016_snapshot.json to reflect updated database schema with new tables and relationships.

---
+++ lib/db/migrations/0016_goofy_spectrum.sql
@@ -0,0 +1,4 @@
+DROP TABLE "Document" CASCADE;--> statement-breakpoint
+DROP TABLE "Message" CASCADE;--> statement-breakpoint
+DROP TABLE "Suggestion" CASCADE;--> statement-breakpoint
+DROP TABLE "Vote" CASCADE;
